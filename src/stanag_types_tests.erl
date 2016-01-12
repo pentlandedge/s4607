a70077425b9022dbe5f00ea50d619624575c71e1
Robert Forbes <robert.forbes@pentlandedge.com>
2016-01-12 21:35:28 +0000

    Rewrote some of the simple type tests as a test generator function.

--- src/stanag_types_tests.erl
+++ src/stanag_types_tests.erl
@@ -2,11 +2,19 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-i8_test() ->
-    255 = stanag_types:i8_to_integer(<<255>>),    
-    250 = stanag_types:i8_to_integer(<<250>>),    
-    0   = stanag_types:i8_to_integer(<<0>>),
+%% Define a test generator for the unsigned integer types.
+unsigned_test_() ->
+    [i8_checks(), i16_checks()].
+
+i8_checks() ->
+    [?_assertEqual(255, stanag_types:i8_to_integer(<<255>>)),    
+     ?_assertEqual(250, stanag_types:i8_to_integer(<<250>>)),    
+     ?_assertEqual(0, stanag_types:i8_to_integer(<<0>>)),
     
-    <<255>> = stanag_types:integer_to_i8(255),
-    <<250>> = stanag_types:integer_to_i8(250),
-    <<0>>   = stanag_types:integer_to_i8(0).
+     ?_assertEqual(<<255>>, stanag_types:integer_to_i8(255)),
+     ?_assertEqual(<<250>>, stanag_types:integer_to_i8(250)),
+     ?_assertEqual(<<0>>, stanag_types:integer_to_i8(0))].
+
+i16_checks() ->
+    [?_assertEqual(255, stanag_types:i16_to_integer(<<0,255>>)),
+     ?_assertEqual(65280, stanag_types:i16_to_integer(<<255,0>>))]. 
