47bc945e0f48bfd3306ea7760c23430ab5470c96
Robert Forbes <robert.forbes@pentlandedge.com>
2016-01-13 20:30:26 +0000

    Added automated I32 type tests.

--- src/stanag_types_tests.erl
+++ src/stanag_types_tests.erl
@@ -4,7 +4,7 @@
 
 %% Define a test generator for the unsigned integer types.
 unsigned_test_() ->
-    [i8_checks(), i16_checks()].
+    [i8_checks(), i16_checks(), i32_checks()].
 
 i8_checks() ->
     [?_assertEqual(255, stanag_types:i8_to_integer(<<255>>)),    
@@ -22,3 +22,11 @@ i16_checks() ->
      ?_assertEqual(<<0,255>>, stanag_types:integer_to_i16(255)),
      ?_assertEqual(<<255,0>>, stanag_types:integer_to_i16(65280)),
      ?_assertEqual(<<255,255>>, stanag_types:integer_to_i16(65535))]. 
+
+i32_checks() ->
+    [?_assertEqual(255, stanag_types:i32_to_integer(<<0,0,0,255>>)),
+     ?_assertEqual(4294967295, stanag_types:i32_to_integer(<<255,255,255,255>>)),
+     ?_assertEqual(16909060, stanag_types:i32_to_integer(<<1,2,3,4>>)),
+
+     ?_assertEqual(<<1,2,3,4>>, stanag_types:integer_to_i32(16909060)),
+     ?_assertEqual(<<0,0,255,255>>, stanag_types:integer_to_i32(65535))]. 
