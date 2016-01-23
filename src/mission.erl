72b5b7e4b679d091bdabc90fea0a52556eda7d97
Robert Forbes <robert.forbes@pentlandedge.com>
2016-01-23 14:42:35 +0000

    Moved the mission segment decoding to its own file.

---
+++ src/mission.erl
@@ -0,0 +1,111 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Copyright 2016 Pentland Edge Ltd.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+%% use this file except in compliance with the License. 
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software 
+%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+%% License for the specific language governing permissions and limitations 
+%% under the License.
+%%
+-module(mission).
+
+-export([
+    decode/1, 
+    display/1, 
+    decode_platform_type/1,
+    mission_test/0]).
+
+-record(mission_segment, {
+    mission_plan, 
+    flight_plan, 
+    platform_type,
+    platform_config,
+    year,
+    month,
+    day}).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Mission segment decoding functions.
+
+decode(<<M1:12/binary, M2:12/binary, M3, M4:10/binary, 
+    Year:16/integer-unsigned-big, Month, Day>>) ->
+
+    Mission = trim_trailing_spaces(binary_to_list(M1)),
+    Flight = trim_trailing_spaces(binary_to_list(M2)),
+    Type = decode_platform_type(M3),
+    Config = trim_trailing_spaces(binary_to_list(M4)),
+
+    #mission_segment{mission_plan = Mission, flight_plan = Flight, 
+        platform_type = Type, platform_config = Config, year = Year,
+        month = Month, day = Day}.
+
+decode_platform_type(0) -> unidentfified;
+decode_platform_type(1) -> acs;
+decode_platform_type(2) -> arl_m;
+decode_platform_type(3) -> sentinel;
+decode_platform_type(4) -> rotary_wing_radar;
+decode_platform_type(5) -> global_hawk_navy;
+decode_platform_type(6) -> horizon;
+decode_platform_type(7) -> e_8;
+decode_platform_type(8) -> p_3c;
+decode_platform_type(9) -> predator;
+decode_platform_type(10) -> radarsat2;
+decode_platform_type(11) -> u_2;
+decode_platform_type(12) -> e_10;
+decode_platform_type(13) -> ugs_single;
+decode_platform_type(14) -> ugs_cluster;
+decode_platform_type(15) -> ground_based;
+decode_platform_type(16) -> uav_army;
+decode_platform_type(17) -> uav_marines;
+decode_platform_type(18) -> uav_navy;
+decode_platform_type(19) -> uav_air_force;
+decode_platform_type(20) -> global_hawk_air_force;
+decode_platform_type(21) -> global_hawk_australia;
+decode_platform_type(22) -> global_hawk_germany;
+decode_platform_type(23) -> paul_revere;
+decode_platform_type(24) -> mariner_uav;
+decode_platform_type(25) -> bac_111;
+decode_platform_type(26) -> coyote;
+decode_platform_type(27) -> king_air;
+decode_platform_type(28) -> limit;
+decode_platform_type(29) -> nrl_np_3b;
+decode_platform_type(30) -> solstar_x;
+decode_platform_type(31) -> watchkeeper;
+decode_platform_type(32) -> alliance_ground_surveillance;
+decode_platform_type(33) -> stryker;
+decode_platform_type(34) -> ags_hale_uav;
+decode_platform_type(35) -> sidm;
+decode_platform_type(36) -> reaper;
+decode_platform_type(37) -> warrior_a;
+decode_platform_type(38) -> warrior;
+decode_platform_type(39) -> twin_otter;
+decode_platform_type(255) -> other;
+decode_platform_type(_) -> future_use.
+
+mission_test() ->
+    <<"Global Domin","Fly By      ",36,"Skynet v12",16#07, 16#DF, 12, 31>>.
+
+display(MSeg) ->
+    io:format("Mission Plan: ~p~n", [MSeg#mission_segment.mission_plan]),
+    io:format("Flight Plan: ~p~n", [MSeg#mission_segment.flight_plan]),
+    io:format("Plat. Type: ~p~n", [MSeg#mission_segment.platform_type]),
+    io:format("Plat. Config: ~p~n", [MSeg#mission_segment.platform_config]),
+    io:format("Year: ~p~n", [MSeg#mission_segment.year]),
+    io:format("Month: ~p~n", [MSeg#mission_segment.month]),
+    io:format("Day: ~p~n", [MSeg#mission_segment.day]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Utility functions
+
+trim_trailing_spaces(Str) ->
+    Rev = lists:reverse(Str),
+    F = fun(C) -> C =:= $\  end,
+    RStrip = lists:dropwhile(F, Rev),
+    lists:reverse(RStrip).
+
--- src/s4607.erl
+++ src/s4607.erl
@@ -34,9 +34,6 @@
     get_platform_id/1,
     get_mission_id/1,
     get_job_id/1,
-    decode_mission_segment/1,
-    display_mission_segment/1,
-    mission_test/0,
     decode_dwell_segment/1,
     display_dwell_segment/1,
     display_existence_mask/1,
@@ -59,16 +56,7 @@
     mission_id, 
     job_id}).
 
--record(segment, {header, data}).
-
--record(mission_segment, {
-    mission_plan, 
-    flight_plan, 
-    platform_type,
-    platform_config,
-    year,
-    month,
-    day}).
+%-record(segment, {header, data}).
 
 -record(exist_mask, {
     revisit_index,
@@ -204,7 +192,7 @@
     terr_elev_model,
     geoid_model}).
 
--record(decode_status, {status, error_list}).
+%-record(decode_status, {status, error_list}).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% File handling functions.
@@ -257,7 +245,7 @@ decode_segments(Bin, Acc) ->
     % Switch on the segment type
     case seg_header:get_segment_type(SH) of
         mission -> 
-            SegRec = {ok, SH, decode_mission_segment(SegData)};
+            SegRec = {ok, SH, mission:decode(SegData)};
         dwell   ->
             SegRec = {ok, SH, decode_dwell_segment(SegData)};
         job_definition ->
@@ -313,8 +301,8 @@ display_segments(Bin) ->
     % Switch on the segment type
     case seg_header:get_segment_type(SH) of
         mission -> 
-            MS = decode_mission_segment(SegData),
-            display_mission_segment(MS);
+            MS = mission:decode(SegData),
+            mission:display(MS);
         dwell   ->
             DS = decode_dwell_segment(SegData),
             display_dwell_segment(DS);
@@ -336,7 +324,7 @@ display_segment(SegHdr, SegRec) ->
     % Switch on the segment type and display the segment data.
     case seg_header:get_segment_type(SegHdr) of
         mission -> 
-            display_mission_segment(SegRec);
+            mission:display(SegRec);
         dwell   ->
             display_dwell_segment(SegRec);
         job_definition ->
@@ -488,76 +476,6 @@ get_mission_id(#pheader{mission_id = X}) -> X.
 
 %% Get the job ID from the header structure.
 get_job_id(#pheader{job_id = X}) -> X.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Mission segment decoding functions.
-
-decode_mission_segment(<<M1:12/binary, M2:12/binary, M3, M4:10/binary, 
-    Year:16/integer-unsigned-big, Month, Day>>) ->
-
-    Mission = trim_trailing_spaces(binary_to_list(M1)),
-    Flight = trim_trailing_spaces(binary_to_list(M2)),
-    Type = decode_platform_type(M3),
-    Config = trim_trailing_spaces(binary_to_list(M4)),
-
-    #mission_segment{mission_plan = Mission, flight_plan = Flight, 
-        platform_type = Type, platform_config = Config, year = Year,
-        month = Month, day = Day}.
-
-decode_platform_type(0) -> unidentfified;
-decode_platform_type(1) -> acs;
-decode_platform_type(2) -> arl_m;
-decode_platform_type(3) -> sentinel;
-decode_platform_type(4) -> rotary_wing_radar;
-decode_platform_type(5) -> global_hawk_navy;
-decode_platform_type(6) -> horizon;
-decode_platform_type(7) -> e_8;
-decode_platform_type(8) -> p_3c;
-decode_platform_type(9) -> predator;
-decode_platform_type(10) -> radarsat2;
-decode_platform_type(11) -> u_2;
-decode_platform_type(12) -> e_10;
-decode_platform_type(13) -> ugs_single;
-decode_platform_type(14) -> ugs_cluster;
-decode_platform_type(15) -> ground_based;
-decode_platform_type(16) -> uav_army;
-decode_platform_type(17) -> uav_marines;
-decode_platform_type(18) -> uav_navy;
-decode_platform_type(19) -> uav_air_force;
-decode_platform_type(20) -> global_hawk_air_force;
-decode_platform_type(21) -> global_hawk_australia;
-decode_platform_type(22) -> global_hawk_germany;
-decode_platform_type(23) -> paul_revere;
-decode_platform_type(24) -> mariner_uav;
-decode_platform_type(25) -> bac_111;
-decode_platform_type(26) -> coyote;
-decode_platform_type(27) -> king_air;
-decode_platform_type(28) -> limit;
-decode_platform_type(29) -> nrl_np_3b;
-decode_platform_type(30) -> solstar_x;
-decode_platform_type(31) -> watchkeeper;
-decode_platform_type(32) -> alliance_ground_surveillance;
-decode_platform_type(33) -> stryker;
-decode_platform_type(34) -> ags_hale_uav;
-decode_platform_type(35) -> sidm;
-decode_platform_type(36) -> reaper;
-decode_platform_type(37) -> warrior_a;
-decode_platform_type(38) -> warrior;
-decode_platform_type(39) -> twin_otter;
-decode_platform_type(255) -> other;
-decode_platform_type(_) -> future_use.
-
-mission_test() ->
-    <<"Global Domin","Fly By      ",36,"Skynet v12",16#07, 16#DF, 12, 31>>.
-
-display_mission_segment(MSeg) ->
-    io:format("Mission Plan: ~p~n", [MSeg#mission_segment.mission_plan]),
-    io:format("Flight Plan: ~p~n", [MSeg#mission_segment.flight_plan]),
-    io:format("Plat. Type: ~p~n", [MSeg#mission_segment.platform_type]),
-    io:format("Plat. Config: ~p~n", [MSeg#mission_segment.platform_config]),
-    io:format("Year: ~p~n", [MSeg#mission_segment.year]),
-    io:format("Month: ~p~n", [MSeg#mission_segment.month]),
-    io:format("Day: ~p~n", [MSeg#mission_segment.day]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Dwell segment decoding functions.
