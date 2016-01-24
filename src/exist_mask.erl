3ea987d3b1954f3d8083731c01c88acab9d45b8d
Robert Forbes <robert.forbes@pentlandedge.com>
2016-01-24 20:08:02 +0000

    Added accessor functions for the existence mask fields.

--- src/exist_mask.erl
+++ src/exist_mask.erl
@@ -17,10 +17,55 @@
 
 -export([
     decode/1, 
-    display/1
-    
-    
-    ]).
+    display/1,
+    get_revisit_index/1,
+    get_dwell_index/1,
+    get_last_dwell_of_revisit/1,
+    get_target_report_count/1,
+    get_dwell_time/1,
+    get_sensor_lat/1,
+    get_sensor_lon/1,
+    get_sensor_alt/1,
+    get_lat_scale_factor/1,
+    get_lon_scale_factor/1,
+    get_spu_along_track/1,
+    get_spu_cross_track/1,
+    get_spu_alt/1,
+    get_sensor_track/1,
+    get_sensor_speed/1,
+    get_sensor_vert_vel/1,
+    get_sensor_track_unc/1,
+    get_sensor_speed_unc/1,
+    get_sensor_vert_vel_unc/1,
+    get_platform_heading/1,
+    get_platform_pitch/1,
+    get_platform_roll/1,
+    get_dwell_center_lat/1,
+    get_dwell_center_lon/1,
+    get_dwell_range_half_extent/1,
+    get_dwell_angle_half_extent/1,
+    get_sensor_heading/1,
+    get_sensor_pitch/1,
+    get_sensor_roll/1,
+    get_mdv/1,
+    get_mti_report_index/1,
+    get_target_hr_lat/1,
+    get_target_hr_lon/1,
+    get_target_delta_lat/1,
+    get_target_delta_lon/1,
+    get_geodetic_height/1,
+    get_target_vel_los/1,
+    get_target_wrap_velocity/1,
+    get_target_snr/1,
+    get_target_classification/1,
+    get_target_class_prob/1,
+    get_target_slant_range_unc/1,
+    get_target_cross_range_unc/1,
+    get_target_height_unc/1,
+    get_target_rad_vel_unc/1,
+    get_truth_tag_app/1,
+    get_truth_tag_entity/1,
+    get_target_rcs/1]).
 
 -record(exist_mask, {
     revisit_index,
@@ -181,5 +226,56 @@ display(EM) ->
     io:format("Truth tag app.: ~p~n", [EM#exist_mask.truth_tag_app]),
     io:format("Truth tag entity: ~p~n", [EM#exist_mask.truth_tag_entity]),
     io:format("Target RCS: ~p~n", [EM#exist_mask.target_rcs]).
+
+%% Functions to allow external modules to gain access to fields of the 
+%% existence mask without knowledge of the internal representation.
+get_revisit_index(EM) -> EM#exist_mask.revisit_index.
+get_dwell_index(EM) -> EM#exist_mask.dwell_index.
+get_last_dwell_of_revisit(EM) -> EM#exist_mask.last_dwell_of_revisit.
+get_target_report_count(EM) -> EM#exist_mask.target_report_count.
+get_dwell_time(EM) -> EM#exist_mask.dwell_time.
+get_sensor_lat(EM) -> EM#exist_mask.sensor_lat.
+get_sensor_lon(EM) -> EM#exist_mask.sensor_lon.
+get_sensor_alt(EM) -> EM#exist_mask.sensor_alt.
+get_lat_scale_factor(EM) -> EM#exist_mask.lat_scale_factor.
+get_lon_scale_factor(EM) -> EM#exist_mask.lon_scale_factor.
+get_spu_along_track(EM) -> EM#exist_mask.spu_along_track.
+get_spu_cross_track(EM) -> EM#exist_mask.spu_cross_track.
+get_spu_alt(EM) -> EM#exist_mask.spu_alt.
+get_sensor_track(EM) -> EM#exist_mask.sensor_track.
+get_sensor_speed(EM) -> EM#exist_mask.sensor_speed.
+get_sensor_vert_vel(EM) -> EM#exist_mask.sensor_vert_vel.
+get_sensor_track_unc(EM) -> EM#exist_mask.sensor_track_unc.
+get_sensor_speed_unc(EM) -> EM#exist_mask.sensor_speed_unc.
+get_sensor_vert_vel_unc(EM) -> EM#exist_mask.sensor_vert_vel_unc.
+get_platform_heading(EM) -> EM#exist_mask.platform_heading.
+get_platform_pitch(EM) -> EM#exist_mask.platform_pitch.
+get_platform_roll(EM) -> EM#exist_mask.platform_roll.
+get_dwell_center_lat(EM) -> EM#exist_mask.dwell_center_lat.
+get_dwell_center_lon(EM) -> EM#exist_mask.dwell_center_lon.
+get_dwell_range_half_extent(EM) -> EM#exist_mask.dwell_range_half_extent.
+get_dwell_angle_half_extent(EM) -> EM#exist_mask.dwell_angle_half_extent.
+get_sensor_heading(EM) -> EM#exist_mask.sensor_heading.
+get_sensor_pitch(EM) -> EM#exist_mask.sensor_pitch.
+get_sensor_roll(EM) -> EM#exist_mask.sensor_roll.
+get_mdv(EM) -> EM#exist_mask.mdv.
+get_mti_report_index(EM) -> EM#exist_mask.mti_report_index.
+get_target_hr_lat(EM) -> EM#exist_mask.target_hr_lat.
+get_target_hr_lon(EM) -> EM#exist_mask.target_hr_lon.
+get_target_delta_lat(EM) -> EM#exist_mask.target_delta_lat.
+get_target_delta_lon(EM) -> EM#exist_mask.target_delta_lon.
+get_geodetic_height(EM) -> EM#exist_mask.geodetic_height.
+get_target_vel_los(EM) -> EM#exist_mask.target_vel_los.
+get_target_wrap_velocity(EM) -> EM#exist_mask.target_wrap_velocity.
+get_target_snr(EM) -> EM#exist_mask.target_snr.
+get_target_classification(EM) -> EM#exist_mask.target_classification.
+get_target_class_prob(EM) -> EM#exist_mask.target_class_prob.
+get_target_slant_range_unc(EM) -> EM#exist_mask.target_slant_range_unc.
+get_target_cross_range_unc(EM) -> EM#exist_mask.target_cross_range_unc.
+get_target_height_unc(EM) -> EM#exist_mask.target_height_unc.
+get_target_rad_vel_unc(EM) -> EM#exist_mask.target_rad_vel_unc.
+get_truth_tag_app(EM) -> EM#exist_mask.truth_tag_app.
+get_truth_tag_entity(EM) -> EM#exist_mask.truth_tag_entity.
+get_target_rcs(EM) -> EM#exist_mask.target_rcs.
 
 
