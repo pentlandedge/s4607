2c6e994556d4d30245f4cfaf7d953e188b2daf03
Robert Forbes <robert.forbes@pentlandedge.com>
2016-01-24 20:38:27 +0000

    Updated the remaining modules to use the existence mask accessor functions and removed the duplicate record definitions.

--- src/tgt_report.erl
+++ src/tgt_report.erl
@@ -37,181 +37,130 @@
     truth_tag_entity,
     target_rcs}).
 
--record(exist_mask, {
-    revisit_index,
-    dwell_index,
-    last_dwell_of_revisit,
-    target_report_count,
-    dwell_time,
-    sensor_lat,
-    sensor_lon,
-    sensor_alt,
-    lat_scale_factor,
-    lon_scale_factor,
-    spu_along_track,
-    spu_cross_track,
-    spu_alt,
-    sensor_track,
-    sensor_speed,
-    sensor_vert_vel,
-    sensor_track_unc,
-    sensor_speed_unc,
-    sensor_vert_vel_unc,
-    platform_heading,
-    platform_pitch,
-    platform_roll,
-    dwell_center_lat,
-    dwell_center_lon,
-    dwell_range_half_extent,
-    dwell_angle_half_extent,
-    sensor_heading,
-    sensor_pitch,
-    sensor_roll,
-    mdv,
-    mti_report_index,
-    target_hr_lat,
-    target_hr_lon,
-    target_delta_lat,
-    target_delta_lon,
-    geodetic_height,
-    target_vel_los,
-    target_wrap_velocity,
-    target_snr,
-    target_classification,
-    target_class_prob,
-    target_slant_range_unc,
-    target_cross_range_unc,
-    target_height_unc,
-    target_rad_vel_unc,
-    truth_tag_app,
-    truth_tag_entity,
-    target_rcs}).
-
-
 decode(TrBin, EM) ->
     
     {MRI, Rem1} = sutils:conditional_extract(
         TrBin, 
-        EM#exist_mask.mti_report_index, 
+        exist_mask:get_mti_report_index(EM), 
         2, 
         fun stanag_types:i16_to_integer/1, 
         0),
 
     {TgtHiResLat, Rem2} = sutils:conditional_extract(
         Rem1, 
-        EM#exist_mask.target_hr_lat, 
+        exist_mask:get_target_hr_lat(EM), 
         4, 
         fun stanag_types:sa32_to_float/1, 
         0.0),
 
     {TgtHiResLon, Rem3} = sutils:conditional_extract(
         Rem2, 
-        EM#exist_mask.target_hr_lon, 
+        exist_mask:get_target_hr_lon(EM), 
         4, 
         fun stanag_types:ba32_to_float/1, 
         0.0),
 
     {TgtDeltaLat, Rem4} = sutils:conditional_extract(
         Rem3, 
-        EM#exist_mask.target_delta_lat, 
+        exist_mask:get_target_delta_lat(EM), 
         2, 
         fun stanag_types:s16_to_integer/1, 
         0),
 
     {TgtDeltaLon, Rem5} = sutils:conditional_extract(
         Rem4, 
-        EM#exist_mask.target_delta_lon, 
+        exist_mask:get_target_delta_lon(EM), 
         2, 
         fun stanag_types:s16_to_integer/1, 
         0),
 
     {GeodHeight, Rem6} = sutils:conditional_extract(
         Rem5, 
-        EM#exist_mask.geodetic_height, 
+        exist_mask:get_geodetic_height(EM), 
         2, 
         fun stanag_types:s16_to_integer/1, 
         0),
 
     {TgtVelLos, Rem7} = sutils:conditional_extract(
         Rem6, 
-        EM#exist_mask.target_vel_los, 
+        exist_mask:get_target_vel_los(EM), 
         2, 
         fun stanag_types:s16_to_integer/1, 
         0),
 
     {TgtWrapVel, Rem8} = sutils:conditional_extract(
         Rem7, 
-        EM#exist_mask.target_wrap_velocity, 
+        exist_mask:get_target_wrap_velocity(EM), 
         2, 
         fun stanag_types:i16_to_integer/1, 
         0),
 
     {TgtSnr, Rem9} = sutils:conditional_extract(
         Rem8, 
-        EM#exist_mask.target_snr, 
+        exist_mask:get_target_snr(EM), 
         1, 
         fun stanag_types:s8_to_integer/1, 
         0),
 
     {TgtClassification, Rem10} = sutils:conditional_extract(
         Rem9, 
-        EM#exist_mask.target_classification, 
+        exist_mask:get_target_classification(EM), 
         1, 
         fun decode_target_classification/1, 
         no_information_live_target),
 
     {TgtClassProb, Rem11} = sutils:conditional_extract(
         Rem10, 
-        EM#exist_mask.target_class_prob, 
+        exist_mask:get_target_class_prob(EM), 
         1, 
         fun stanag_types:i8_to_integer/1, 
         0),
 
     {TgtSlantRgeUnc, Rem12} = sutils:conditional_extract(
         Rem11, 
-        EM#exist_mask.target_slant_range_unc, 
+        exist_mask:get_target_slant_range_unc(EM), 
         2, 
         fun stanag_types:i16_to_integer/1, 
         0),
 
     {TgtCrossRgeUnc, Rem13} = sutils:conditional_extract(
         Rem12, 
-        EM#exist_mask.target_cross_range_unc, 
+        exist_mask:get_target_cross_range_unc(EM), 
         2, 
         fun stanag_types:i16_to_integer/1, 
         0),
 
     {TgtHeightUnc, Rem14} = sutils:conditional_extract(
         Rem13, 
-        EM#exist_mask.target_height_unc, 
+        exist_mask:get_target_height_unc(EM), 
         1, 
         fun stanag_types:i8_to_integer/1, 
         0),
 
     {TgtRadVelUnc, Rem15} = sutils:conditional_extract(
         Rem14, 
-        EM#exist_mask.target_rad_vel_unc, 
+        exist_mask:get_target_rad_vel_unc(EM), 
         2, 
         fun stanag_types:i16_to_integer/1, 
         0),
 
     {TruthTagApp, Rem16} = sutils:conditional_extract(
         Rem15, 
-        EM#exist_mask.truth_tag_app, 
+        exist_mask:get_truth_tag_app(EM), 
         1, 
         fun stanag_types:i8_to_integer/1, 
         0),
 
     {TruthTagEnt, Rem17} = sutils:conditional_extract(
         Rem16, 
-        EM#exist_mask.truth_tag_entity, 
+        exist_mask:get_truth_tag_entity(EM), 
         4, 
         fun stanag_types:i32_to_integer/1, 
         0),
 
     {TgtRcs, Rem18} = sutils:conditional_extract(
         Rem17, 
-        EM#exist_mask.target_rcs, 
+        exist_mask:get_target_rcs(EM), 
         1, 
         fun stanag_types:s8_to_integer/1, 
         0),
@@ -277,23 +226,22 @@ decode_target_classification(255) -> unknown_simulated_target;
 decode_target_classification(_) -> reserved.
  
 display(TR, EM) ->
-    sutils:conditional_display("MTI report index: ~p~n", [TR#tgt_report.mti_report_index], EM#exist_mask.mti_report_index),
-    sutils:conditional_display("Target HR Lat: ~p~n", [TR#tgt_report.target_hr_lat], EM#exist_mask.target_hr_lat),
-    sutils:conditional_display("Target HR Lon: ~p~n", [TR#tgt_report.target_hr_lon], EM#exist_mask.target_hr_lon),
-    sutils:conditional_display("Target Delta Lat: ~p~n", [TR#tgt_report.target_delta_lat], EM#exist_mask.target_delta_lat),
-    sutils:conditional_display("Target Delta Lon: ~p~n", [TR#tgt_report.target_delta_lon], EM#exist_mask.target_delta_lon),
-    sutils:conditional_display("Geodetic Height: ~p~n", [TR#tgt_report.geodetic_height], EM#exist_mask.geodetic_height),
-    sutils:conditional_display("Target vel. LOS.: ~p~n", [TR#tgt_report.target_vel_los], EM#exist_mask.target_vel_los),
-    sutils:conditional_display("Target wrap vel.: ~p~n", [TR#tgt_report.target_wrap_velocity], EM#exist_mask.target_wrap_velocity),
-    sutils:conditional_display("Target SNR: ~p~n", [TR#tgt_report.target_snr], EM#exist_mask.target_snr),
-    sutils:conditional_display("Target classification: ~p~n", [TR#tgt_report.target_classification], EM#exist_mask.target_classification),
-    sutils:conditional_display("Target classification probability: ~p~n", [TR#tgt_report.target_class_prob], EM#exist_mask.target_class_prob),
-    sutils:conditional_display("Target slant range unc.: ~p~n", [TR#tgt_report.target_slant_range_unc], EM#exist_mask.target_slant_range_unc),
-    sutils:conditional_display("Target cross range unc.: ~p~n", [TR#tgt_report.target_cross_range_unc], EM#exist_mask.target_cross_range_unc),
-    sutils:conditional_display("Target height unc.: ~p~n", [TR#tgt_report.target_height_unc], EM#exist_mask.target_height_unc),
-    sutils:conditional_display("Target rad. vel. unc.: ~p~n", [TR#tgt_report.target_rad_vel_unc], EM#exist_mask.target_rad_vel_unc),
-    sutils:conditional_display("Truth tag application: ~p~n", [TR#tgt_report.truth_tag_app], EM#exist_mask.truth_tag_app),
-    sutils:conditional_display("Truth tag entity: ~p~n", [TR#tgt_report.truth_tag_entity], EM#exist_mask.truth_tag_entity),
-    sutils:conditional_display("Target RCS: ~p~n", [TR#tgt_report.target_rcs], EM#exist_mask.target_rcs).
-
+    sutils:conditional_display("MTI report index: ~p~n", [TR#tgt_report.mti_report_index], exist_mask:get_mti_report_index(EM)),
+    sutils:conditional_display("Target HR Lat: ~p~n", [TR#tgt_report.target_hr_lat], exist_mask:get_target_hr_lat(EM)),
+    sutils:conditional_display("Target HR Lon: ~p~n", [TR#tgt_report.target_hr_lon], exist_mask:get_target_hr_lon(EM)),
+    sutils:conditional_display("Target Delta Lat: ~p~n", [TR#tgt_report.target_delta_lat], exist_mask:get_target_delta_lat(EM)),
+    sutils:conditional_display("Target Delta Lon: ~p~n", [TR#tgt_report.target_delta_lon], exist_mask:get_target_delta_lon(EM)),
+    sutils:conditional_display("Geodetic Height: ~p~n", [TR#tgt_report.geodetic_height], exist_mask:get_geodetic_height(EM)),
+    sutils:conditional_display("Target vel. LOS.: ~p~n", [TR#tgt_report.target_vel_los], exist_mask:get_target_vel_los(EM)),
+    sutils:conditional_display("Target wrap vel.: ~p~n", [TR#tgt_report.target_wrap_velocity], exist_mask:get_target_wrap_velocity(EM)),
+    sutils:conditional_display("Target SNR: ~p~n", [TR#tgt_report.target_snr], exist_mask:get_target_snr(EM)),
+    sutils:conditional_display("Target classification: ~p~n", [TR#tgt_report.target_classification], exist_mask:get_target_classification(EM)),
+    sutils:conditional_display("Target classification probability: ~p~n", [TR#tgt_report.target_class_prob], exist_mask:get_target_class_prob(EM)),
+    sutils:conditional_display("Target slant range unc.: ~p~n", [TR#tgt_report.target_slant_range_unc], exist_mask:get_target_slant_range_unc(EM)),
+    sutils:conditional_display("Target cross range unc.: ~p~n", [TR#tgt_report.target_cross_range_unc], exist_mask:get_target_cross_range_unc(EM)),
+    sutils:conditional_display("Target height unc.: ~p~n", [TR#tgt_report.target_height_unc], exist_mask:get_target_height_unc(EM)),
+    sutils:conditional_display("Target rad. vel. unc.: ~p~n", [TR#tgt_report.target_rad_vel_unc], exist_mask:get_target_rad_vel_unc(EM)),
+    sutils:conditional_display("Truth tag application: ~p~n", [TR#tgt_report.truth_tag_app], exist_mask:get_truth_tag_app(EM)),
+    sutils:conditional_display("Truth tag entity: ~p~n", [TR#tgt_report.truth_tag_entity], exist_mask:get_truth_tag_entity(EM)),
+    sutils:conditional_display("Target RCS: ~p~n", [TR#tgt_report.target_rcs], exist_mask:get_target_rcs(EM)).
 
