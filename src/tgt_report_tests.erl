5296d39ed2ef91e4f94d3260d4f1fe8cb43d9faf
Robert Forbes <robert.forbes@pentlandedge.com>
2016-01-29 18:24:53 +0000

    Added the first target report unit tests.

--- src/tgt_report_tests.erl
+++ src/tgt_report_tests.erl
@@ -20,6 +20,24 @@
 
 %% Define a test generator for target reports. 
 tgt_report_test_() ->
-    [].
+    [tgt_checks1()].
 
+tgt_checks1() ->
+    Params = [{mti_report_index, 34}, {target_hr_lat, -33.3}, 
+              {target_hr_lon, -3.57}, {target_delta_lat, -45},
+              {target_delta_lon, 46}, {geodetic_height, 5000},
+              {target_vel_los, 32000}, {target_wrap_velocity, 40000},
+              {target_snr, -128}, {target_classification, vehicle_live_target},
+              {target_class_prob, 90}, {target_slant_range_unc, 1000},
+              {target_cross_range_unc, 2000}, {target_height_unc, 200},
+              {target_rad_vel_unc, 5000}, {truth_tag_app, 200},
+              {truth_tag_entity, 10000}, {target_rcs, 44.5}],
+
+    R1 = tgt_report:new(Params),
+
+    [?_assertEqual(34, tgt_report:get_mti_report_index(R1)),
+     ?_assertEqual(46, tgt_report:get_target_delta_lon(R1))].
+
+
+    
 
