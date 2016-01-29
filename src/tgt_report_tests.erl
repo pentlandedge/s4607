807be3bee93f0b0838f506468c3464cca0e4dbaf
Robert Forbes <robert.forbes@pentlandedge.com>
2016-01-29 18:45:18 +0000

    Finished adding checks for constructing all the fields of a target report.

--- src/tgt_report_tests.erl
+++ src/tgt_report_tests.erl
@@ -31,13 +31,31 @@ tgt_checks1() ->
               {target_class_prob, 90}, {target_slant_range_unc, 1000},
               {target_cross_range_unc, 2000}, {target_height_unc, 200},
               {target_rad_vel_unc, 5000}, {truth_tag_app, 200},
-              {truth_tag_entity, 10000}, {target_rcs, 44.5}],
+              {truth_tag_entity, 10000}, {target_rcs, 10}],
 
     R1 = tgt_report:new(Params),
 
     [?_assertEqual(34, tgt_report:get_mti_report_index(R1)),
-     ?_assertEqual(46, tgt_report:get_target_delta_lon(R1))].
+     ?_assert(almost_equal(-33.3, tgt_report:get_target_hr_lat(R1), 0.0000001)),
+     ?_assert(almost_equal(-3.57, tgt_report:get_target_hr_lon(R1), 0.0000001)),
+     ?_assertEqual(46, tgt_report:get_target_delta_lon(R1)),
+     ?_assertEqual(5000, tgt_report:get_geodetic_height(R1)),
+     ?_assertEqual(32000, tgt_report:get_target_vel_los(R1)),
+     ?_assertEqual(40000, tgt_report:get_target_wrap_velocity(R1)),
+     ?_assertEqual(-128, tgt_report:get_target_snr(R1)),
+     ?_assertEqual(vehicle_live_target, tgt_report:get_target_classification(R1)),
+     ?_assertEqual(90, tgt_report:get_target_class_prob(R1)),
+     ?_assertEqual(1000, tgt_report:get_target_slant_range_unc(R1)),
+     ?_assertEqual(2000, tgt_report:get_target_cross_range_unc(R1)),
+     ?_assertEqual(200, tgt_report:get_target_height_unc(R1)),
+     ?_assertEqual(5000, tgt_report:get_target_rad_vel_unc(R1)),
+     ?_assertEqual(200, tgt_report:get_truth_tag_app(R1)),
+     ?_assertEqual(10000, tgt_report:get_truth_tag_entity(R1)),
+     ?_assertEqual(10, tgt_report:get_target_rcs(R1))].
 
 
+%% Utility function to compare whether floating point values are within a 
+%% specified range.
+almost_equal(V1, V2, Delta) ->
+    abs(V1 - V2) =< Delta.
     
-
