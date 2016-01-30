58282658ae61a2c8baf9880e0792d4c22f9aec68
Robert Forbes <robert.forbes@pentlandedge.com>
2016-01-30 15:01:05 +0000

    Added a placeholder for the dwell segment unit tests.

--- src/dwell_tests.erl
+++ src/dwell_tests.erl
@@ -14,12 +14,12 @@
 %% under the License.
 %%
 
--module(all_tests).
+-module(dwell_tests).
 
 -include_lib("eunit/include/eunit.hrl").
 
-%% Define a test generator function to run all the tests. 
-all_test_() ->
-    [{module, s4607}, {module, stanag_types}, {module, mission},
-     {module, job_def}, {module, exist_mask}, {module, tgt_report},
-     {module, dwell}].  
+%% Define a test generator for the dwell segment. 
+dwell_test_() ->
+    [].
+
+
