92c6aa6b47afb89ffd8c2565cc082d9029aa4a6d
Robert Forbes <robert.forbes@pentlandedge.com>
2016-01-31 15:51:39 +0000

    Added the remainder of the simple field checks for the minimal dwell.

--- src/dwell_tests.erl
+++ src/dwell_tests.erl
@@ -29,7 +29,10 @@ creation_checks1() ->
      ?_assertEqual(no_additional_dwells, dwell:get_last_dwell_of_revisit(DS)),
      ?_assertEqual(0, dwell:get_target_report_count(DS)),
      ?_assertEqual(1000000, dwell:get_dwell_time(DS)),
+     ?_assert(almost_equal(-45.0, dwell:get_sensor_lat(DS), 0.0000001)),
+     ?_assert(almost_equal(350.0, dwell:get_sensor_lon(DS), 0.0000001)),
      ?_assertEqual(-10000, dwell:get_sensor_alt(DS)),
+     ?_assert(almost_equal(255.0, dwell:get_dwell_range_half_extent(DS), 0.0000001)),
      ?_assertEqual(350, dwell:get_dwell_angle_half_extent(DS))].
 
 %% Function to create a sample dwell segment with only the mandatory fields
@@ -55,3 +58,7 @@ minimal_dwell() ->
     % Use the parameters to construct a new dwell segment.
     dwell:new(P).
 
+%% Utility function to compare whether floating point values are within a 
+%% specified range.
+almost_equal(V1, V2, Delta) ->
+    abs(V1 - V2) =< Delta.
