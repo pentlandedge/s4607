0f81f87b95475fcd8181584cbd9c05046b3dc239
Robert Forbes <robert.forbes@pentlandedge.com>
2016-02-04 12:54:42 +0000

    Added a sample three target dwell segment for testing.

--- src/dwell_tests.erl
+++ src/dwell_tests.erl
@@ -18,7 +18,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--export([minimal_dwell/0, one_target_dwell/0]).
+-export([minimal_dwell/0, one_target_dwell/0, three_targets_dwell/0]).
 
 %% Define a test generator for the dwell segment. 
 dwell_test_() ->
@@ -189,6 +189,74 @@ one_target_dwell() ->
          {sensor_alt, -10000}, {dwell_center_lat, -45.2}, 
          {dwell_center_lon, 350.2}, {dwell_range_half_extent, 255.0}, 
          {dwell_angle_half_extent, 350}, {targets, [TgtRep]}],
+
+    % Create and return the dwell segment.
+    dwell:new(P).
+
+%% Function to create a sample dwell with three target reports in it.
+three_targets_dwell() ->
+    % Create a list of fields for the existence mask (excluding the target
+    % report).
+    F = [existence_mask, revisit_index, dwell_index, last_dwell_of_revisit,
+         target_report_count, dwell_time, sensor_lat, sensor_lon, 
+         sensor_alt, dwell_center_lat, dwell_center_lon, 
+         dwell_range_half_extent, dwell_angle_half_extent, targets],
+  
+    % The fields of the target report 1.
+    Params1 = [{mti_report_index, 34}, {target_hr_lat, -33.3}, 
+               {target_hr_lon, 357.57}, {target_delta_lat, -45},
+               {target_delta_lon, 46}, {geodetic_height, 3000},
+               {target_vel_los, 32000}, {target_wrap_velocity, 40000},
+               {target_snr, -128}, {target_classification, vehicle_live_target},
+               {target_class_prob, 90}, {target_slant_range_unc, 1000},
+               {target_cross_range_unc, 2000}, {target_height_unc, 200},
+               {target_rad_vel_unc, 5000}, {truth_tag_app, 200},
+               {truth_tag_entity, 10000}, {target_rcs, 10}],
+
+    % The fields of the target report 2.
+    Params2 = [{mti_report_index, 34}, {target_hr_lat, -33.4}, 
+               {target_hr_lon, 357.67}, {target_delta_lat, -45},
+               {target_delta_lon, 46}, {geodetic_height, 4000},
+               {target_vel_los, 32000}, {target_wrap_velocity, 40000},
+               {target_snr, -128}, {target_classification, vehicle_live_target},
+               {target_class_prob, 90}, {target_slant_range_unc, 1000},
+               {target_cross_range_unc, 2000}, {target_height_unc, 200},
+               {target_rad_vel_unc, 5000}, {truth_tag_app, 200},
+               {truth_tag_entity, 10000}, {target_rcs, 15}],
+
+    % The fields of the target report 3.
+    Params3 = [{mti_report_index, 34}, {target_hr_lat, -33.5}, 
+               {target_hr_lon, 357.77}, {target_delta_lat, -45},
+               {target_delta_lon, 46}, {geodetic_height, 5000},
+               {target_vel_los, 32000}, {target_wrap_velocity, 40000},
+               {target_snr, -128}, {target_classification, vehicle_live_target},
+               {target_class_prob, 90}, {target_slant_range_unc, 1000},
+               {target_cross_range_unc, 2000}, {target_height_unc, 200},
+               {target_rad_vel_unc, 5000}, {truth_tag_app, 200},
+               {truth_tag_entity, 10000}, {target_rcs, 20}],
+
+    % Extract the list of fields in the target report.
+    FieldList = [K || {K, _V} <- Params1],
+
+    % Splice together all the fields that make up the existence mask.
+    Efields = F ++ FieldList,
+
+    % Create the existence mask.
+    EM = exist_mask:new(Efields), 
+   
+    % Create the target reports.
+    TgtRep1 = tgt_report:new(Params1),
+    TgtRep2 = tgt_report:new(Params2),
+    TgtRep3 = tgt_report:new(Params3),
+
+    % Set the fields of the dwell segment.
+    P = [{existence_mask, EM}, {revisit_index, 100}, {dwell_index, 20000}, 
+         {last_dwell_of_revisit, no_additional_dwells}, {target_report_count, 3}, 
+         {dwell_time, 1000000}, {sensor_lat, -45.0}, {sensor_lon, 350},
+         {sensor_alt, -10000}, {dwell_center_lat, -45.2}, 
+         {dwell_center_lon, 350.2}, {dwell_range_half_extent, 255.0}, 
+         {dwell_angle_half_extent, 350}, 
+         {targets, [TgtRep1, TgtRep2, TgtRep3]}],
 
     % Create and return the dwell segment.
     dwell:new(P).
