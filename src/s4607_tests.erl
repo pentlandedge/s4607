ac88ead7c026da8e7ffb8ed91ddfab77154de2bd
Robert Forbes <robert.forbes@pentlandedge.com>
2016-02-16 16:43:48 +0000

    Added a job definition packet encode/decode test.

--- src/s4607_tests.erl
+++ src/s4607_tests.erl
@@ -21,7 +21,7 @@
 s4607_test_() ->
     [encode_mission_segment_check(), encode_job_def_segment_check(),
      encode_dwell_segment_check1(), encode_dwell_segment_check2(),
-     mission_packet_encode()].
+     mission_packet_encode(), job_def_packet_encode()].
    
 encode_mission_segment_check() ->
     % Create a mission segment.
@@ -140,3 +140,62 @@ mission_packet_encode() ->
      ?_assertEqual(mission, seg_header:get_segment_type(SegHdr)),
      ?_assertEqual(44, seg_header:get_segment_size(SegHdr)),
      ?_assertEqual(other, mission:get_platform_type(MissSeg))].
+
+job_def_packet_encode() ->
+
+    % List the parameters for the packet header (no need to set size). 
+    PL = [{version, {3, 1}}, {nationality, "UK"},
+          {classification, top_secret}, {class_system, "UK"}, 
+          {packet_code, rel_9_eyes}, {exercise_ind, operation_real},
+          {platform_id, "Pico1"}, {mission_id, 16#11223344},
+          {job_id, 16#55667788}],
+
+    % Create a generator function
+    Gen = s4607:packet_generator(PL),
+
+    % Create a sample job definition segment to test with.
+    JD = job_def_tests:sample_job_def(),
+
+    % Create a complete segment with the header and payload.
+    Seg = segment:new(job_definition, JD),
+
+    % Create a packet with the new segment.
+    Pack = Gen([Seg]), 
+    
+    % Encode the packet.
+    EP = s4607:encode_packet(Pack),
+
+    % Decode it again.
+    [DP] = s4607:decode(EP),
+
+    % Pull out the individual records.
+    DPH = s4607:get_packet_header(DP),
+    [DPS] = s4607:get_packet_segments(DP),
+    SegHdr = segment:get_header(DPS),
+    DEJD = segment:get_data(DPS),
+
+    [?_assertEqual(105, pheader:get_packet_size(DPH)),
+     ?_assertEqual(job_definition, seg_header:get_segment_type(SegHdr)),
+     ?_assertEqual(73, seg_header:get_segment_size(SegHdr)),
+     ?_assertEqual(100, job_def:get_job_id(DEJD)),
+     ?_assertEqual(rotary_wing_radar, job_def:get_sensor_id_type(DEJD)),
+     ?_assertEqual("Heli 1", job_def:get_sensor_id_model(DEJD)),
+     ?_assertEqual(no_filtering, job_def:get_target_filt_flag(DEJD)),
+     ?_assertEqual(30, job_def:get_priority(DEJD)),
+     ?_assertEqual({monopulse_calibration, asars_aip}, job_def:get_radar_mode(DEJD)),
+     ?_assertEqual(65000, job_def:get_nom_rev_int(DEJD)),
+     ?_assertEqual(no_statement, job_def:get_ns_pos_unc_along_track(DEJD)),
+     ?_assertEqual(5000, job_def:get_ns_pos_unc_cross_track(DEJD)),
+     ?_assertEqual(20000, job_def:get_ns_pos_unc_alt(DEJD)),
+     ?_assertEqual(45, job_def:get_ns_pos_unc_heading(DEJD)),
+     ?_assertEqual(65534, job_def:get_ns_pos_unc_sensor_speed(DEJD)),
+     ?_assertEqual(100, job_def:get_ns_val_slant_range_std_dev(DEJD)),
+     ?_assertEqual(no_statement, job_def:get_ns_val_cross_range_std_dev(DEJD)),
+     ?_assertEqual(4000, job_def:get_ns_val_tgt_vel_los_std_dev(DEJD)),
+     ?_assertEqual(no_statement, job_def:get_ns_val_mdv(DEJD)),
+     ?_assertEqual(100, job_def:get_ns_val_det_prob(DEJD)),
+     ?_assertEqual(254, job_def:get_ns_val_false_alarm_density(DEJD)),
+     ?_assertEqual(dgm50, job_def:get_terr_elev_model(DEJD)),
+     ?_assertEqual(geo96, job_def:get_geoid_model(DEJD))].
+
+
