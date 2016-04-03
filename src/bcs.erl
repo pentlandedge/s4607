ede9760ed502785f19a34c8f538e7de850c51f4f
Robert Forbes <robert.forbes@pentlandedge.com>
2016-04-03 19:07:11 +0100

    Added a skeleton for developing BCS (Basic Character Set) functions.

---
+++ src/bcs.erl
@@ -0,0 +1,24 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Copyright 2016 Pentland Edge Ltd.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+%% use this file except in compliance with the License. 
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software 
+%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+%% License for the specific language governing permissions and limitations 
+%% under the License.
+%% 
+%% This module contains functions relating the the BCS (Basic Character Set)
+%% defined in the Stanag 4607 standard.
+
+-module(bcs).
+
+-export([is_valid/1]).
+
+%% Function to test whether a character belongs to the BCS set.
+is_valid(_Char) -> true.
