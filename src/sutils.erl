8eb31bb5cee1331e75395989f64e2645be7fce50
Henrik <mau.henrik@gmail.com>
2016-04-10 20:07:22 +0100

    Added helper function to extract data and apply function to data before returning it

--- src/sutils.erl
+++ src/sutils.erl
@@ -2,15 +2,15 @@
 %% Copyright 2016 Pentland Edge Ltd.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License"); you may not
-%% use this file except in compliance with the License. 
+%% use this file except in compliance with the License.
 %% You may obtain a copy of the License at
 %%
 %% http://www.apache.org/licenses/LICENSE-2.0
 %%
-%% Unless required by applicable law or agreed to in writing, software 
-%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
-%% License for the specific language governing permissions and limitations 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+%% License for the specific language governing permissions and limitations
 %% under the License.
 %%
 -module(sutils).
@@ -20,7 +20,8 @@
     add_trailing_spaces/2,
     conditional_extract/5,
     conditional_display/3,
-    extract_data/2]).
+    extract_data/2,
+    extract_conv_data/3]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Utility functions
@@ -45,7 +46,7 @@ add_trailing_spaces(Str, N) when length(Str) =:= N ->
 %% based on the state of a mask bit.
 conditional_extract(Bin, MaskBit, Size, ConvFn, Default) ->
     case MaskBit of
-        1 -> 
+        1 ->
             {ok, Param, Bin1} = extract_data(Bin, Size),
             {ConvFn(Param), Bin1};
         0 ->
@@ -68,4 +69,10 @@ extract_data(Bin, Len) ->
     Rem = binary:part(Bin, Len, (byte_size(Bin) - Len)),
     {ok, Data, Rem}.
 
+%% Generic function to take the first part of a binary, apply a function
+%% and return the rest.
+extract_conv_data(Bin, Len, ConvFn) ->
+    Data = binary:part(Bin, 0, Len),
+    Rem = binary:part(Bin, Len, (byte_size(Bin) - Len)),
+    {ok, ConvFn(Data), Rem}.
 
