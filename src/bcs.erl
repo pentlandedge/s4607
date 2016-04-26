4f77dc06a457380afa1ef958e633af8c2671eedb
Robert Forbes <robert.forbes@pentlandedge.com>
2016-04-26 12:44:25 +0100

    Added @doc comments to the bcs module.

--- src/bcs.erl
+++ src/bcs.erl
@@ -13,14 +13,14 @@
 %% License for the specific language governing permissions and limitations 
 %% under the License.
 %% 
-%% This module contains functions relating the the BCS (Basic Character Set)
-%% defined in the Stanag 4607 standard.
+%% @doc This module contains functions relating the the BCS (Basic Character 
+%% Set) defined in the Stanag 4607 standard.
 
 -module(bcs).
 
 -export([is_valid_string/1, is_valid_char/1]).
 
-%% Function to test whether a string contains only valid BCS characters.
+%% @doc Test whether a string contains only valid BCS characters.
 is_valid_string([]) -> 
     true;
 is_valid_string([H|T]) ->
@@ -29,7 +29,7 @@ is_valid_string([H|T]) ->
         false -> false
     end.
 
-%% Function to test whether a character belongs to the BCS set.
+%% @doc Test whether a character belongs to the BCS set.
 %% Allows the caller to provide a character as a single element string or as
 %% an integer
 is_valid_char(16#0A) -> true;   % Line feed
