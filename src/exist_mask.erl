d258c826e27b89b6129f8073c8a2714e8ee57ff3
Robert Forbes <robert.forbes@pentlandedge.com>
2016-05-23 22:16:24 +0100

    Started adding type specs to the target report module.

--- src/exist_mask.erl
+++ src/exist_mask.erl
@@ -119,6 +119,9 @@
     truth_tag_entity,
     target_rcs}).
 
+-opaque exist_mask() :: #exist_mask{}.
+-export_type([exist_mask/0]).
+
 %% Function to decode the existance mask. Will crash caller if the mask 
 %% does not have the mandatory bits set.
 decode(<<16#FF, 
