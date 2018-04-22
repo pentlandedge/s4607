21a8fa13e941e98e100dd249559d71cbecea3379
Robert Forbes <robert.forbes@pentlandedge.com>
2018-04-22 22:59:54 +0100

    Added the Osprey, Seaspray and PicoSAR mode definitions to the type specifier.

--- src/job_def.erl
+++ src/job_def.erl
@@ -156,7 +156,10 @@
     {hrr_mti_spot_scan, lynx_i_ii} | {hrr_mti_arc_scan, lynx_i_ii} | 
     {grca, global_hawk} | {rrca, global_hawk} | {gmti_hrr, global_hawk} | 
     {small_area_gmti, vader} | {wide_area_gmti, vader} | 
-    {dismount_gmti, vader} | {hrr_gmti, vader}.
+    {dismount_gmti, vader} | {hrr_gmti, vader} |{mti, osprey} | 
+    {mti_ss, osprey} | {e_mti, osprey} | {e_mti_hi_bw, osprey} | 
+    {mti, picosar} | {e_mti, picosar} | {e_mti_hi_bw, picosar} | 
+    {maritime_search, picosar} | {mti, seaspray} | {mti_ss, seaspray}.
 
 -export_type([sensor_id_type/0, radar_mode/0]).
 
